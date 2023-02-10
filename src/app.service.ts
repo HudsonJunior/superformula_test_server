7266ba6dd00e54c21ae9e0804c8a7dec02c17dad
Hudson <devhudsoncontact@gmail.com>
2023-02-10 16:23:30 -0300

    Adding the expires at property correctly

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,11 +3,13 @@ import { Body, Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getSeed(): Object {
+    const date = new Date();
+    date.setMinutes(date.getMinutes() + 1);
     return {
       description: "seed generated",
       content: {
         seed: (Math.random() + 1).toString(36).substring(2),
-        expires_at: Date(),
+        expires_at: date.toISOString(),
       },
     };
   }
